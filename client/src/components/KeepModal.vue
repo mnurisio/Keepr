113996123e23af5e29313cf666c7386906fd8062
Mike Nurisio <michaelnurisio@gmail.com>
2025-01-25 14:57:03 -0700

    built out keep modal, started on vaultkeeping component

--- client/src/components/KeepModal.vue
+++ client/src/components/KeepModal.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { AppState } from '@/AppState';
+import { Keep } from '@/models/Keep';
 import { computed } from 'vue';
 
 
@@ -8,18 +9,44 @@ import { computed } from 'vue';
 const keep = computed(() => AppState.activeKeep)
 
 
+
 </script>
 
 
 <template>
-    <div class="container-fluid modal fade " id="keepModal" tabindex="-1" aria-labelledby="keepModalLabel">
+    <div class="container-fluid modal fade" id="keepModal" tabindex="-1">
         <div class="modal-dialog modal-lg">
             <div v-if="keep" class="modal-content">
-                <div class="row">
-                    <div class="col-md-6">
+                <div class="row w-100 h-100">
+                    <div class="col-md-6 p-0">
                         <img class="img-fluid w-100 h-100" :src="keep.img" :alt="keep.name">
                     </div>
-                    <div class="col-6">
+                    <div class="col-md-6 p-0 modal-info">
+                        <div class="row justify-content-center text-center">
+                            <div class="col-4 p-1">
+                                <span class="m-2"><i class="mdi mdi-eye-outline"></i> {{ keep.views }}</span>
+                                <span class="m-2"><i class="mdi mdi-pin-outline"></i> {{ keep.kept }}</span>
+                            </div>
+                        </div>
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1>{{ keep.name }}</h1>
+                            </div>
+                            <div class="text-start">
+                                <p>{{ keep.description }}</p>
+                            </div>
+                        </div>
+                        <div class="row ps-5">
+                            <div class="col-md-6">
+                                hello
+                            </div>
+                            <div class="col-md-6 text-end">
+                                <div class="p-2">
+                                    <img :src="keep.creator.picture" :alt="keep.creator.name" class="profile-pic me-2">
+                                    <span>{{ keep.creator.name }}</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -28,4 +55,25 @@ const keep = computed(() => AppState.activeKeep)
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+    object-fit: cover;
+    object-position: center;
+}
+
+.modal-content {
+    overflow: hidden;
+}
+
+.modal-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.profile-pic{
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+    height:2.5em;
+}
+</style>
